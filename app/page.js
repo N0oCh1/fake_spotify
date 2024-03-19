7cc7553abfaee7cfb9bb659d783ee20a76374fa9
Ruben Feng <rfeng@ayudinga.org>
2024-03-18 19:16:49 -0500

    🐛 : use current url

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-
+'use client'
 import style from "./styles/app.module.css"
 import AsideTopvar from "./component/AsideTopVar";
 import AsideVar from "./component/AsideVar";
@@ -6,15 +6,17 @@ import AsidePlaylist from "./component/AsidePlaylist";
 import { Suspense } from "react";
 import Login from "./component/Login";
 import GetPlaylist from "./component/GetPlaylist";
+import { useUrl } from "nextjs-current-url";
 
 
 export default function Home() {
-
+  const { href: currentUrl} = useUrl() ?? {};
   const {spotyStyle, listContent} = style
   const clientID = "e9e63f77d41a40b4bcec61711746eedc";
   const clientSecret = "45e34c3a7b524e409fa5e97d97830bda";
   const scope = "playlist-read-private playlist-read-collaborative"
-  const redirectURL = "http://localhost:3000/"
+  const redirectURL = currentUrl;
+  console.log(redirectURL)
   return (
     <div className= {`${spotyStyle} ralative h-screen gap-2 p-2 flex bg-gray-950`}>
       <aside className="[grid-area:aside] bg-black flex flex-col gap-2 max-h-screen">
